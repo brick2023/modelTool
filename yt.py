a71f49295d144343eff812352ba722645de21510
yenslife <77geo5rge6@gmail.com>
2023-07-03 18:44:24 +0800

    docs & test: add issue ref link, add 台大 algorithm video download example
    修改：新增下載範例(台大演算法)、以及發現 pytube 的 issue，如果有問題可以參考[這個連結](https://github.com/pytube/pytube/issues/1678)

--- yt.py
+++ yt.py
@@ -3,6 +3,7 @@
 pip requirement:
 pip install pytube
 pip install pydub
+如果遇到 pytube issue 可以參考：https://github.com/pytube/pytube/issues/1678
 '''
 from pytube import YouTube 
 from pydub import AudioSegment
@@ -51,6 +52,38 @@ def yt_url_to_mp4(url, output_path='./', filename='yt-video.mp4'):
     print('影片下載完成')
 
 # test
+if __name__=='__main__':
+    # 台大演算法課程
+    company1_video_name_url={'Lec1 演算法':'https://youtu.be/7a28lIQIHJQ',
+                            'Lec2 演算法':'https://youtu.be/kxIQ86qMtfk',
+                            'Lec3 演算法':'https://youtu.be/OVhJHBYJLSE',
+                            'Lec4 演算法':'https://youtu.be/rP2wm4bEfXo',
+                            'Lec5 演算法':'https://youtu.be/tTFolqL1TX4',
+                            'Lec6 演算法':'https://youtu.be/24akv3pVt28',
+                            'Lec7 演算法':'https://youtu.be/FvUs-OlAy18',
+                            'Lec8 演算法':'https://youtu.be/r5CTlHdo9Oo',
+                            'Lec9 演算法':'https://youtu.be/OQDaruIDCjY',
+                            'Lec10 演算法':'https://youtu.be/hZjp35QI-40',
+                            'Lec11 演算法':'https://youtu.be/0UGaXukC4b0',
+                            'Lec12 演算法':'https://youtu.be/yWZHGsd06dA',
+                            'Lec13 演算法':'',
+                            'Lec14 演算法':'',
+                            'Lec15 演算法':'',
+                            'Lec16 演算法':'',
+                            'Lec17 演算法':'',
+                            'Lec18 演算法':'',
+                            'Lec19 演算法':'',
+                            'Lec20 演算法':'',
+                            'Lec21 演算法':'',
+                            'Lec22 演算法':'',
+                            'Lec23 演算法':'',
+                            'Lec24 演算法':'',}
+    for name, url in company1_video_name_url.items():
+        if url != '':
+            name = name.split(' ')[0]
+            yt_url_to_mp4(url, output_path='/home/brick/platform/src/video/company1/algorithm/', filename=name+'.mp4')
+            print(name, url)
+
 # print(yt_url_to_mp3('https://youtu.be/FZYh6lPymJ0'))
 # yt_url_to_mp4('https://youtu.be/vxxPtDCb9Go', output_path='./test-video/', filename='ai-class1.mp4')
 # yt_url_to_mp4('https://youtu.be/VpKN3KvSK6c', output_path='./test-video/', filename='ai-class2.mp4')
